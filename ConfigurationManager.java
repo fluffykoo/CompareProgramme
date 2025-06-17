ab619960f8af94446342faf19dc9b77e4d31e7b1
fluffykoo <oumou.camara@efrei.net>
2025-06-18 00:44:30 +0200

    Update ConfigurationManager.java

--- ConfigurationManager.java
+++ ConfigurationManager.java
@@ -1,3 +1,28 @@
 package com.mmd;
 
-// ... (code omitted for brevity; see previous user message) ...
+import com.google.gson.*;
+import java.io.*;
+
+public class ConfigurationManager {
+    private String clePrincipale;
+    private String cleSecondaire;
+    private JsonObject subSectionKeys;
+    
+    public ConfigurationManager(String fichierConfig) throws IOException {
+        chargerConfiguration(fichierConfig);
+    }
+    
+    private void chargerConfiguration(String fichierConfig) throws IOException {
+        JsonObject config = JsonParser.parseReader(new FileReader(fichierConfig)).getAsJsonObject();
+        
+        this.clePrincipale = config.get("primary_key").getAsString();
+        this.cleSecondaire = config.has("fallback_key") ? 
+            config.get("fallback_key").getAsString() : null;
+        this.subSectionKeys = config.getAsJsonObject("subSectionKeys");
+    }
+    
+    // Getters
+    public String getClePrincipale() { return clePrincipale; }
+    public String getCleSecondaire() { return cleSecondaire; }
+    public JsonObject getSubSectionKeys() { return subSectionKeys; }
+}
