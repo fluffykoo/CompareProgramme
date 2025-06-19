d6e29dc8b43c6f05dfc72e5f60c70ff1f8182ffa
fluffykoo <oumou.camara@efrei.net>
2025-06-19 10:34:33 +0200

    Update CompareTxtFiles.java

--- CompareTxtFiles.java
+++ CompareTxtFiles.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class CompareTxtFiles {
     public static void main(String[] args) throws IOException {
         if (args.length < 4) {
-            System.out.println("Usage : java CompareTxtFiles <fichier1> <fichier2> <indexCle> <dossierRapport> [terminal]");
+            System.out.println("Usage: java CompareTxtFiles <referenceFile> <newFile> <keyColumnIndex> <reportFolder> [terminal]");
             return;
         }
 
@@ -18,28 +18,28 @@ public class CompareTxtFiles {
         String dossierRapport = args[3];
 
         TxtComparator comparator = new TxtComparator(afficherDansTerminal);
+
         comparator.runComparison(fichier1, fichier2, indexCle);
 
         TxtReportGenerator generator = new TxtReportGenerator(afficherDansTerminal);
         generator.generateReports(dossierRapport, comparator.getRapportTexte(), comparator.getXlsxData());
 
-        int totalAjout = (int) comparator.getXlsxData().stream().filter(l -> "AJOUT".equals(l[0])).count();
-        int totalSupp = (int) comparator.getXlsxData().stream().filter(l -> "DELETION".equals(l[0])).count();
-        int totalModif = (int) comparator.getXlsxData().stream().filter(l -> "MODIFICATION".equals(l[0])).map(l -> l[1]).distinct().count();
-        int totalIdentique = comparator.getTotalIdentique();
+        int totalAjout = comparator.getXlsxData().stream().filter(l -> "AJOUT".equals(l[0])).toArray().length;
+        int totalSupp = comparator.getXlsxData().stream().filter(l -> "DELETION".equals(l[0])).toArray().length;
+        int totalModif = comparator.getXlsxData().stream().filter(l -> "MODIFICATION".equals(l[0])).map(l -> l[1]).collect(Collectors.toSet()).size();
+        int totalIdentique = comparator.getTotalIdentique(); // méthode à ajouter
 
-        String nomFichierTxt = "comparison_result.txt";
-        String nomFichierXlsx = "comparison_result.xlsx";
-
-        System.out.println();
-        System.out.println("=== Summary ===");
-        System.out.println("Reference file       : " + fichier1);
-        System.out.println("New file             : " + fichier2);
-        System.out.println("Text (.txt) report   : " + dossierRapport + "/" + nomFichierTxt);
-        System.out.println("Excel (.xlsx) report : " + dossierRapport + "/" + nomFichierXlsx);
-        System.out.println("* Identical rows     : " + totalIdentique);
-        System.out.println("* Modified rows      : " + totalModif);
-        System.out.println("* Added rows         : " + totalAjout);
-        System.out.println("* Deleted rows       : " + totalSupp);
+        if (!afficherDansTerminal) {
+            System.out.println();
+            System.out.println("=== Summary ===");
+            System.out.println("Reference file : " + fichier1);
+            System.out.println("New file       : " + fichier2);
+            System.out.println("* Text (.txt) report generated   : " + generator.getTxtFilePath());
+            System.out.println("* Excel (.xlsx) report generated : " + generator.getXlsxFilePath());
+            System.out.println("* Identical rows : " + totalIdentique);
+            System.out.println("* Modified rows  : " + totalModif);
+            System.out.println("* Added rows     : " + totalAjout);
+            System.out.println("* Deleted rows   : " + totalSupp);
+        }
     }
 }
