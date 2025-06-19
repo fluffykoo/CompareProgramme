1e7b49a5c8bc29ce2f421526f4b1aedc32cee904
fluffykoo <oumou.camara@efrei.net>
2025-06-19 15:45:30 +0200

    Update TxtReportGenerator.java

--- TxtReportGenerator.java
+++ TxtReportGenerator.java
@@ -1,133 +1,53 @@
-package com.mmd;
-
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+package com.mmd.txt;
 
 import java.io.*;
 import java.nio.file.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Arrays;
+import java.util.*;
 
 public class TxtReportGenerator {
-    private final boolean afficherDansTerminal;
-    private String txtFilePath;
-    private String xlsxFilePath;
+    private String dossierSortie;
+    private String horodatage;
 
-    public TxtReportGenerator(boolean afficherDansTerminal) {
-        this.afficherDansTerminal = afficherDansTerminal;
+    public TxtReportGenerator(String dossierSortie, String horodatage) {
+        this.dossierSortie = dossierSortie;
+        this.horodatage = horodatage;
     }
 
-    private void afficher(String ligne) {
-        if (afficherDansTerminal) System.out.println(ligne);
-    }
+    public String genererRapportTexte(Map<String, String> refMap, Map<String, String> newMap) throws IOException {
+        String chemin = dossierSortie + "/comparison_result_" + horodatage + ".txt";
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get(chemin));
 
-    public void generateReports(String dossierRapport, StringBuilder rapportTexte, List<String[]> xlsxData) throws IOException {
-        String horodatage = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
-        Files.createDirectories(Paths.get(dossierRapport));
+        int identical = 0, modified = 0, added = 0, deleted = 0;
 
-        String txtPath = Paths.get(dossierRapport, "rapportTXT_" + horodatage + ".txt").toString();
-        txtFilePath = txtPath;
-        Files.write(Paths.get(txtPath), rapportTexte.toString().getBytes());
-
-        afficher("* Text (.txt) report saved to : " + txtPath);
-
-        if (afficherDansTerminal) {
-            afficher("\n=== Full Text Report ===");
-            for (String ligne : rapportTexte.toString().split("\n")) {
-                System.out.println(ligne);
-            }
-            afficher("");
-            afficher("* Text (.txt) report generated : " + txtPath);
-        } else {
-            long totalAjout = xlsxData.stream().filter(l -> "AJOUT".equals(l[0])).count();
-            long totalSupp = xlsxData.stream().filter(l -> "DELETION".equals(l[0])).count();
-            long totalModif = xlsxData.stream().filter(l -> "MODIFICATION".equals(l[0])).map(l -> l[1]).distinct().count();
-            long totalIdentique = Arrays.stream(rapportTexte.toString().split("\\R"))
-                                        .filter(l -> l.startsWith("[UNCHANGED]"))
-                                        .count();
-
-            afficher("");
-            afficher("=== Summary ===");
-            afficher("* Identical rows     : " + totalIdentique);
-            afficher("* Modified rows      : " + totalModif);
-            afficher("* Added rows         : " + totalAjout);
-            afficher("* Deleted rows       : " + totalSupp);
-        }
-
-        exportToXlsx(dossierRapport, horodatage, xlsxData);
-    }
-
-    private void exportToXlsx(String dossierRapport, String horodatage, List<String[]> xlsxData) throws IOException {
-        String nomFichier = "rapportTXT_" + horodatage + ".xlsx";
-        Path cheminFichier = Paths.get(dossierRapport, nomFichier);
-        xlsxFilePath = cheminFichier.toString();
-
-        Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet("Differences");
-
-        String[] titres = {"Type", "Key", "Changed field", "Old value", "New value"};
-        CellStyle headerStyle = workbook.createCellStyle();
-        headerStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-        Row header = sheet.createRow(0);
-        for (int i = 0; i < titres.length; i++) {
-            Cell cell = header.createCell(i);
-            cell.setCellValue(titres[i]);
-            cell.setCellStyle(headerStyle);
-        }
-
-        CellStyle ajoutStyle = workbook.createCellStyle();
-        ajoutStyle.setFillForegroundColor(IndexedColors.LIGHT_GREEN.getIndex());
-        ajoutStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-        CellStyle suppStyle = workbook.createCellStyle();
-        suppStyle.setFillForegroundColor(IndexedColors.ROSE.getIndex());
-        suppStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-        CellStyle modifStyle = workbook.createCellStyle();
-        modifStyle.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex());
-        modifStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-        int rowNum = 1;
-        for (String[] ligne : xlsxData) {
-            Row row = sheet.createRow(rowNum++);
-            for (int i = 0; i < ligne.length; i++) {
-                row.createCell(i).setCellValue(ligne[i] != null ? ligne[i] : "");
-            }
-
-            Cell typeCell = row.getCell(0);
-            switch (typeCell.getStringCellValue().toUpperCase()) {
-                case "AJOUT":
-                case "ADD":
-                    typeCell.setCellStyle(ajoutStyle);
-                    break;
-                case "SUPPRESSION":
-                case "DELETION":
-                    typeCell.setCellStyle(suppStyle);
-                    break;
-                case "MODIFICATION":
-                    typeCell.setCellStyle(modifStyle);
-                    break;
+        for (String key : refMap.keySet()) {
+            if (newMap.containsKey(key)) {
+                if (refMap.get(key).equals(newMap.get(key))) {
+                    identical++;
+                } else {
+                    modified++;
+                    writer.write("[MODIFIED] " + key + "\n");
+                }
+            } else {
+                deleted++;
+                writer.write("[DELETED] " + key + "\n");
             }
         }
 
-        try (FileOutputStream fileOut = new FileOutputStream(cheminFichier.toFile())) {
-            workbook.write(fileOut);
+        for (String key : newMap.keySet()) {
+            if (!refMap.containsKey(key)) {
+                added++;
+                writer.write("[ADDED] " + key + "\n");
+            }
         }
-        workbook.close();
 
-        afficher("* .xlsx report generated in " + dossierRapport + " : " + nomFichier);
-        afficher("* Excel (.xlsx) report generated : " + cheminFichier);
-    }
+        writer.write("\n=== Summary ===\n");
+        writer.write("Identical : " + identical + "\n");
+        writer.write("Modified  : " + modified + "\n");
+        writer.write("Added     : " + added + "\n");
+        writer.write("Deleted   : " + deleted + "\n");
+        writer.close();
 
-    public String getTxtFilePath() {
-        return txtFilePath;
-    }
-
-    public String getXlsxFilePath() {
-        return xlsxFilePath;
+        System.out.println("* Text (.txt) report generated: " + chemin);
+        return chemin;
     }
 }
