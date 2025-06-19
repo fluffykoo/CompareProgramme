a0ef496cc3af50d52c6cf5a922d649b470f07136
fluffykoo <oumou.camara@efrei.net>
2025-06-19 16:01:52 +0200

    Update ConfigurationManager.java

--- ConfigurationManager.java
+++ ConfigurationManager.java
@@ -1,78 +1,39 @@
-package com.mmd.json;
+package com.mmd.txt;
 
 import com.google.gson.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
-public class ConfigurationManager {
-    private String primaryKey;
-    private String fallbackKey;
-    private JsonObject subSectionKeys;
-    private JsonObject ignoredFields;
+public class TxtConfigManager {
+    private List<Integer> keyColumns;
+    private List<Integer> ignoredColumns;
 
-    public ConfigurationManager(String configFile) throws IOException {
-        JsonObject config = JsonParser
-                .parseReader(new FileReader(configFile))
-                .getAsJsonObject();
-        this.primaryKey = config.get("primary_key").getAsString();
-        this.fallbackKey = config.has("fallback_key")
-                ? config.get("fallback_key").getAsString()
-                : null;
-        this.subSectionKeys = config.getAsJsonObject("subSectionKeys");
-        this.ignoredFields = config.has("ignored_fields") && config.get("ignored_fields").isJsonObject()
-                ? config.getAsJsonObject("ignored_fields")
-                : new JsonObject();
-    }
+    public TxtConfigManager(String configFilePath) throws IOException {
+        JsonObject config = JsonParser.parseReader(new FileReader(configFilePath)).getAsJsonObject();
 
-    public String getPrimaryKey() { return primaryKey; }
-    public String getFallbackKey() { return fallbackKey; }
-    public JsonObject getSubSectionKeys() { return subSectionKeys; }
+        keyColumns = new ArrayList<>();
+        ignoredColumns = new ArrayList<>();
 
-    public List<String> getSubSectionKeys(String sectionName) {
-        JsonElement keyElement = subSectionKeys.get(sectionName);
-
-        if (keyElement == null || keyElement.isJsonNull()) {
-            return new ArrayList<>();
-        }
-
-        List<String> keys = new ArrayList<>();
-
-        if (keyElement.isJsonPrimitive()) {
-            // clé simple
-            keys.add(keyElement.getAsString());
-        } else if (keyElement.isJsonArray()) {
-            for (JsonElement elem : keyElement.getAsJsonArray()) {
-                keys.add(elem.getAsString());
+        if (config.has("primary_key_columns")) {
+            JsonArray keyArray = config.getAsJsonArray("primary_key_columns");
+            for (JsonElement elem : keyArray) {
+                keyColumns.add(elem.getAsInt());
             }
         }
 
-        return keys;
+        if (config.has("ignored_columns")) {
+            JsonArray ignoredArray = config.getAsJsonArray("ignored_columns");
+            for (JsonElement elem : ignoredArray) {
+                ignoredColumns.add(elem.getAsInt());
+            }
+        }
     }
 
-    public List<String> getIgnoredFields(String section) {
-        if (ignoredFields.has(section)) {
-            JsonArray array = ignoredFields.getAsJsonArray(section);
-            List<String> fields = new ArrayList<>();
-            for (JsonElement elem : array) {
-                fields.add(elem.getAsString());
-            }
-            return fields;
-        }
-        return new ArrayList<>();
+    public List<Integer> getKeyColumns() {
+        return keyColumns;
     }
 
-    public java.util.Map<String, List<String>> getIgnoredFields() {
-        java.util.Map<String, List<String>> result = new java.util.HashMap<>();
-        for (java.util.Map.Entry<String, JsonElement> entry : ignoredFields.entrySet()) {
-            List<String> fields = new ArrayList<>();
-            for (JsonElement elem : entry.getValue().getAsJsonArray()) {
-                fields.add(elem.getAsString());
-            }
-            result.put(entry.getKey(), fields);
-        }
-        return result;
+    public List<Integer> getIgnoredColumns() {
+        return ignoredColumns;
     }
 }
