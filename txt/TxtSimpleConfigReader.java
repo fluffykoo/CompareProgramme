a51a6c4d812ef32c7e0a9ed533a5007a59ec4612
fluffykoo <oumou.camara@efrei.net>
2025-06-30 10:49:03 +0200

    Delimiter TxtSimpleConfigReader.java

--- txt/TxtSimpleConfigReader.java
+++ txt/TxtSimpleConfigReader.java
@@ -1,20 +1,46 @@
 package com.mmd.txt;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Map;
-import com.google.gson.Gson;
+import com.google.gson.*;
+import java.io.*;
+import java.util.*;
 
 public class TxtSimpleConfigReader {
-    public static int getIndexColFromConfig(String cheminConfig) throws IOException {
+    private int indexCol;
+    private Set<Integer> colonnesIgnorees;
+    private String delimiter;
+
+    public TxtSimpleConfigReader(String cheminConfig) throws IOException {
         try (FileReader reader = new FileReader(cheminConfig)) {
             Gson gson = new Gson();
-            Map<?, ?> config = gson.fromJson(reader, Map.class);
+            Map<String, Object> config = gson.fromJson(reader, Map.class);
+
             if (config.containsKey("indexCol")) {
-                return ((Double) config.get("indexCol")).intValue() - 1;
+                this.indexCol = ((Double) config.get("indexCol")).intValue() - 1;
             } else {
-                throw new IllegalArgumentException("Le champ 'indexCol' est manquant dans le fichier config.");
+                throw new IllegalArgumentException("Missing 'indexCol' in config file.");
             }
+
+            this.colonnesIgnorees = new HashSet<>();
+            if (config.containsKey("ignoreColumns")) {
+                List<Double> rawList = (List<Double>) config.get("ignoreColumns");
+                for (Double d : rawList) {
+                    colonnesIgnorees.add(d.intValue() - 1);
+                }
+            }
+
+            this.delimiter = config.containsKey("delimiter") ? config.get("delimiter").toString() : "|";
         }
+    }
+
+    public int getIndexCol() {
+        return indexCol;
+    }
+
+    public Set<Integer> getColonnesIgnorees() {
+        return colonnesIgnorees;
+    }
+
+    public String getDelimiter() {
+        return delimiter;
     }
 }
